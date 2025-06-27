163422b62bd845866ec989d3cc90847335bf98fe
Lukas Knuth <me@lknuth.dev>
2025-06-27 14:29:53 +0200

    New RSS fixtures. Parser coverage now at 100%

--- test/minimalist_reader/feed_parser_test.exs
+++ test/minimalist_reader/feed_parser_test.exs
@@ -4,31 +4,9 @@ defmodule MinimalistReader.FeedParserTest do
   alias MinimalistReader.FeedParser
   alias MinimalistReader.Models.Item
 
-  @test_rss """
-  <?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
-  <channel>
-  <title>RSS Title</title>
-  <description>This is an example of an RSS feed</description>
-  <link>http://www.example.com/main.html</link>
-  <copyright>2020 Example.com All rights reserved</copyright>
-  <lastBuildDate>Mon, 6 Sep 2010 00:01:00 +0000</lastBuildDate>
-  <pubDate>Sun, 6 Sep 2009 16:20:00 +0000</pubDate>
-  <ttl>1800</ttl>
-  <item>
-  <title>Example entry</title>
-  <description>Here is some text containing an interesting description.</description>
-  <link>http://www.example.com/blog/post/1</link>
-  <guid isPermaLink="false">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
-  <pubDate>Sun, 6 Sep 2009 16:20:00 +0000</pubDate>
-  </item>
-  </channel>
-  </rss>
-  """
-
   describe "parse_stream/1 for RSS" do
     test "parses RSS formatted feed" do
-      res = @test_rss |> String.splitter("\n") |> FeedParser.parse_stream()
+      res = FeedParser.parse_stream(load_fixture!("rss/valid_small.xml"))
 
       assert {:ok, [item], []} = res
 
@@ -38,6 +16,19 @@ defmodule MinimalistReader.FeedParserTest do
                link: "http://www.example.com/blog/post/1",
                date: ~U[2009-09-06 16:20:00Z]
              }
+    end
+
+    test "errors if entry has malformed date" do
+      res = FeedParser.parse_stream(load_fixture!("rss/malformed_date.xml"))
+
+      assert {:ok, [], [{0, {:date, _error}}]} = res
+    end
+
+    test "skips entries missing required fields" do
+      res = FeedParser.parse_stream(load_fixture!("rss/incomplete_entries.xml"))
+
+      assert {:ok, [], problems} = res
+      for problem <- problems, do: assert({_idx, :missing_fields} = problem)
     end
   end
 
