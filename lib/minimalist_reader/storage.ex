225dccaf379d120ce367d1a790f9b934f5cb93c6
Lukas Knuth <me@lknuth.dev>
2025-06-28 22:28:54 +0200

    Added GenServer to store Feed items/problems.
    This is currently just a simple GenServer with two lists. I picked this because ETS tables don't really fit the data we're storing, because it's not a key-value structure.
    
    That said, if performance becomes a problem (copying larger lists between processes), we can escalate to use ETS. Not sure if that will ever happen though.

---
+++ lib/minimalist_reader/storage.ex
@@ -0,0 +1,39 @@
+defmodule MinimalistReader.Storage do
+  @moduledoc """
+  An in-memory storage for `MinimalistReader.Models.Item` parsed from
+  feeds and any problems that occurred during the parsing.
+  """
+  use GenServer
+
+  alias MinimalistReader.Models.Item
+
+  defstruct items: [], problems: []
+
+  # coveralls-ignore-start
+  ##### CLIENT ####
+  def start_link(opts), do: GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+
+  def replace(items, problems) when is_list(items) and is_list(problems),
+    do: GenServer.cast(__MODULE__, {:replace, items, problems})
+
+  def feed(%DateTime{} = cutoff), do: GenServer.call(__MODULE__, {:feed, cutoff})
+  def problems, do: GenServer.call(__MODULE__, :problems)
+
+  ##### SERVER ####
+  def init(_opts), do: {:ok, %__MODULE__{}}
+  # coveralls-ignore-stop
+
+  def handle_cast({:replace, items, problems}, state) do
+    {:noreply, %{state | items: items, problems: problems}}
+  end
+
+  def handle_call({:feed, cutoff}, _from, %__MODULE__{items: items} = state) do
+    items
+    |> Enum.filter(fn %Item{date: released} -> DateTime.after?(released, cutoff) end)
+    |> then(&{:reply, &1, state})
+  end
+
+  def handle_call(:problems, _from, %__MODULE__{problems: problems} = state) do
+    {:reply, problems, state}
+  end
+end
---
+++ test/minimalist_reader/storage_test.exs
@@ -0,0 +1,73 @@
+defmodule MinimalistReader.StorageTest do
+  # NOTE: not using "async" because the GenServer has a global `name` registered
+  use ExUnit.Case
+
+  alias MinimalistReader.Models.Item
+  alias MinimalistReader.Storage
+
+  describe "replace/2" do
+    test "updates items and problems in one operation" do
+      items = [
+        make_item("itemA", ~U[2020-10-23 14:00:00Z]),
+        make_item("itemB", ~U[2020-10-23 17:00:00Z])
+      ]
+
+      problems = ["something went wrong", "something else, too"]
+
+      assert {:noreply, %Storage{items: ^items, problems: ^problems}} =
+               Storage.handle_cast({:replace, items, problems}, %Storage{})
+    end
+
+    test "does not retain previous items/problems" do
+      state = %Storage{
+        items: [make_item("itemA", ~U[2020-10-23 14:00:00Z])],
+        problems: ["something went wrong"]
+      }
+
+      items = [make_item("itemB", ~U[2020-10-23 17:00:00Z])]
+      problems = ["something went wrong", "something else, too"]
+
+      assert {:noreply, %Storage{items: ^items, problems: ^problems}} =
+               Storage.handle_cast({:replace, items, problems}, state)
+    end
+  end
+
+  describe "feed/1" do
+    test "returns only items after the cutoff" do
+      state = %Storage{
+        items: [
+          make_item("itemA", ~U[2020-10-23 14:00:00Z]),
+          make_item("itemB", ~U[2020-10-23 17:00:00Z])
+        ]
+      }
+
+      assert {:reply, [%Item{link: "itemB"}], ^state} =
+               Storage.handle_call({:feed, ~U[2020-10-23 14:30:00Z]}, nil, state)
+    end
+
+    test "returns empty list if no items match" do
+      state = %Storage{
+        items: [
+          make_item("itemA", ~U[2020-10-23 14:00:00Z]),
+          make_item("itemB", ~U[2020-10-23 17:00:00Z])
+        ]
+      }
+
+      assert {:reply, [], ^state} =
+               Storage.handle_call({:feed, ~U[2020-10-23 18:00:00Z]}, nil, state)
+    end
+  end
+
+  describe "problems/0" do
+    test "returns all problems from state" do
+      state = %Storage{problems: ["problemA", "problemB"]}
+
+      assert {:reply, ["problemA", "problemB"], ^state} =
+               Storage.handle_call(:problems, nil, state)
+    end
+  end
+
+  defp make_item(id, date) do
+    %Item{date: date, link: id, title: "Mock #{id}", feed: "Test"}
+  end
+end
