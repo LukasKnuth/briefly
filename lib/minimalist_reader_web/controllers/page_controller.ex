f9d8ec2294c0c7811c7e73d596cab6cdbb36832c
Lukas Knuth <me@lknuth.dev>
2025-08-02 14:33:55 +0200

    Display entries by their `group`.

--- lib/minimalist_reader_web/controllers/page_controller.ex
+++ lib/minimalist_reader_web/controllers/page_controller.ex
@@ -5,7 +5,9 @@ defmodule MinimalistReaderWeb.PageController do
   def feed(conn, _params) do
     # TODO Get days ago from param
     # TODO get timezone from user
-    items = MinimalistReader.list_items(0, "Etc/UTC")
-    render(conn, :feed, items: items)
+    MinimalistReader.list_items(0, "Etc/UTC")
+    |> Enum.group_by(& &1.group)
+    |> Enum.sort()
+    |> then(&render(conn, :feed, grouped_items: &1))
   end
 end
